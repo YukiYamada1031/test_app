1efeef259b50b591f788420775ee1eb750e94aec
“YukiYamada1031” <office.deborah.0217@gmail.com>
2026-01-15 00:09:15 +0900

    add about to js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const main = document.querySelector('#mainvisual');
+const textAbout = document.querySelector('.left-about');
+const imageAbout = document.querySelector('.right-about');
 
 main.animate(
   {
@@ -10,3 +12,25 @@ main.animate(
     easing: 'ease',
   }
 );
+
+const show = (entries) => {
+  const keyframes = {
+    opacity: [0,1],
+    translate: ['0 200px', 0],
+  }
+  entries[0].target.animate(keyframes, 2000);
+}
+
+const observer = new IntersectionObserver(show);
+observer.observe(textAbout);
+
+const show2 = (entries) => {
+  const key = {
+    opacity: [0,1],
+    translate: ['150px 0', 0],
+  }
+  entries[0].target.animate(key, 2400);
+}
+
+const observer2 = new IntersectionObserver(show2);
+observer2.observe(imageAbout);
